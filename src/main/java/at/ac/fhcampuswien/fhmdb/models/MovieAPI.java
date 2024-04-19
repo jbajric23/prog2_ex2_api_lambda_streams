81024a26133e077f6fda7c7836fb216f8535afef
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2024-04-19 16:23:59 +0200

    MovieAPI tests added & other tests commented out

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieAPI.java
@@ -66,11 +66,6 @@ public class MovieAPI {
         List<String> mainCast = setListFromJsonElement(movieArray.get("mainCast"));
         String imgUrl = movieArray.get("imgUrl").getAsString();
         Movie movie = new Movie(title, description, genres, releaseYear, rating, lengthInMinutes, directors, writers, mainCast, imgUrl);
-        // Initialize other attributes
-        movie.setLengthInMinutes(movieArray.get("lengthInMinutes").getAsInt());
-        movie.setDirectors(setListFromJsonElement(movieArray.get("directors")));
-        movie.setWriters(setListFromJsonElement(movieArray.get("writers")));
-        movie.setMainCast(setListFromJsonElement(movieArray.get("mainCast")));
         return movie;
     }
 
